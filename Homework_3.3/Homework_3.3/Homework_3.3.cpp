9fa1e59261025418e8e7cfdb21d501ee522815fc
Сергей Селиванов <sergejdog04@gmail.com>
2025-03-17 16:21:20 +0300

    Second attempt

--- Homework_3.3/Homework_3.3/Homework_3.3.cpp
+++ Homework_3.3/Homework_3.3/Homework_3.3.cpp
@@ -10,59 +10,45 @@ public:
     ~Counter() {
         delete[] arr;
     }
-    class getSum {
-    public:
-        getSum(Counter& counter) : counter(counter) {} 
-        int operator()() const { 
-            int sum = 0;
-            for (int i = 0; i < counter.sizeArr; ++i) {
-                if (counter.arr[i] % 3 == 0) {
-                    sum += counter.arr[i];
-                }
-            }
-            return sum;
-        }
-    private:
-        Counter& counter;
-    };
-    class getCount {
-    public:
-        getCount(Counter& counter) : counter(counter) {}
 
-        int operator()() const {
-            int count = 0;
-            for (int i = 0; i < counter.sizeArr; i++) {
-                if (counter.arr[i] % 3 == 0) {
-                    count++;
-                }
+    int getSum() {
+        int sum = 0;
+        for (int i = 0; i < sizeArr; i++) {
+            if (arr[i] % 3 == 0) {
+                sum += arr[i];
             }
-            return count;
         }
-    private:
-        Counter& counter; 
-    };
-
-    getSum getSumFunctor() {
-        return getSum(*this);
+        return sum;
     }
-    getCount getCountFunctor() {
-        return getCount(*this);
+    int getCount() {
+        int count = 0;
+        for (int i = 0; i < sizeArr; i++) {
+            if (arr[i] % 3 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    void operator()() {
+        printArr();
+        std::cout << "[OUT]: get_sum() = " << getSum() << std::endl;
+        std::cout << "[OUT]: get_count() = " << getCount() << std::endl;
     }
 private:
     int* arr;
     int sizeArr;
-};
-void printArr(int* arr,int size){
-    std::cout << "[IN]: ";
-    for (int i = 0; i < size; i++) {
-        std::cout << arr[i] << " ";
+
+    void printArr() {
+        std::cout << "[IN]: ";
+        for (int i = 0; i < sizeArr; i++) {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
-}
+};
 int main() {
-    int arr[] = {4, 1, 3, 6, 25, 55};
-    printArr(arr, sizeof(arr) / sizeof(arr[0]));
+    int arr[] = {4, 1, 3, 6, 25, 54};
     Counter counter(arr,sizeof(arr)/sizeof(arr[0]));
-    std::cout << "[OUT]: get_sum() = " << counter.getSumFunctor()() << std::endl;
-    std::cout << "[OUT]: get_count() = " << counter.getCountFunctor()() << std::endl;
+    counter();
 }
